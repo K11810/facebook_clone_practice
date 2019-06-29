d218ddd3597c8f778bb2f075c2c5385d02be08ba
K_18194 <fastofall6545@gmail.com>
2019-06-29 04:37:15 +0000

    add: home controller and home/top.html. fix: config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'homes#top'
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures do
